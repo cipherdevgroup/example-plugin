08083b9a7b0490f884f0e5f1d45af3f0ebe84c5d
robneu <rob@wpsitecare.com>
2015-02-13 15:04:34 -0600

    Fixed plugin banner

--- grunt/config/usebanner.js
+++ grunt/config/usebanner.js
@@ -4,13 +4,12 @@ module.exports = {
 		options: {
 			position: 'top',
 			banner: '/*!\n' +
-				'Theme Name:  <%= pkg.plugin.name %>\n' +
+				'Plugin Name: <%= pkg.plugin.name %>\n' +
 				'Version:     <%= pkg.version %>\n' +
 				'Author:      <%= pkg.plugin.author %>\n' +
 				'Author URI:  <%= pkg.plugin.authoruri %>\n' +
 				'Theme URI:   <%= pkg.plugin.uri %>\n' +
 				'Description: <%= pkg.plugin.description %>\n' +
-				'Tags:        <%= pkg.plugin.tags %>\n' +
 				'Text Domain: <%= pkg.plugin.textdomain %>\n' +
 				'Domain Path: <%= pkg.plugin.domainpath %>\n' +
 				'License:     <%= pkg.plugin.license %>\n' +
