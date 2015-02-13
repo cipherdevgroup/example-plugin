8a2088e94d4657c3d64c0122f8e2f1938505cd7e
robneu <rob@wpsitecare.com>
2015-02-13 15:04:52 -0600

    Added templates

---
+++ plugin/templates/front-end.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Example Plugin template part for a front end element.
+ *
+ * @package     ExamplePlugin
+ * @author      Robert Neu
+ * @copyright   Copyright (c) 2015, WP Site Care, LLC
+ * @license     GPL-2.0+
+ * @since       0.0.1
+ */
+?>
