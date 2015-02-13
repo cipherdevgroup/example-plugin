128128f3e6599d127b884c0b0bb802148580a673
robneu <rob@wpsitecare.com>
2015-02-13 15:10:03 -0600

    Commented out js by default

--- grunt/config/bowercopy.js
+++ grunt/config/bowercopy.js
@@ -1,27 +1,17 @@
 // https://github.com/timmywil/grunt-bowercopy
 module.exports = {
 	options: {
-		clean: true
+		clean: true,
+		destPrefix: '<%= paths.bower %>'
 	},
 	css: {
-		options: {
-			destPrefix: '<%= paths.bower %>'
-		},
 		files: {
 			bourbon: 'bourbon/app/assets/stylesheets',
 			neat: 'neat/app/assets/stylesheets',
-			genericons: 'genericons/genericons',
 			'wp-normalize': 'wp-normalize.scss/_wp-normalize.scss'
 		}
-	},
-	js: {
-		options: {
-			destPrefix: '<%= paths.bower %>'
-		},
-		files: {
-			'fitvids/js': 'fitvids/jquery.fitvids.js',
-			'accessible-menu/js': 'accessible-menu/dist',
-			'sidr/js': 'sidr/jquery.sidr.min.js'
-		}
 	}
+	//js: {
+	//	files: {}
+	//}
 };
