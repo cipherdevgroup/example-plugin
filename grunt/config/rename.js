7aa3e7290bea303b2c4e8e5eb22ba3642a9dfd50
robneu <rob@wpsitecare.com>
2015-02-25 16:03:45 -0600

    Split up assets renames
    This wasn’t working, but seems alright now

--- grunt/config/rename.js
+++ grunt/config/rename.js
@@ -7,12 +7,20 @@ module.exports = {
 				dest: ['<%= paths.plugin %><%= pkg.name %>.php']
 			},
 			{
-				src: ['<%= paths.authorAssets %>js/example-plugin*'],
+				src: ['<%= paths.authorAssets %>js/example-plugin.js'],
 				dest: ['<%= paths.authorAssets %>js/<%= pkg.name %>.js']
 			},
 			{
-				src: ['<%= paths.authorAssets %>scss/example-plugin*'],
+				src: ['<%= paths.authorAssets %>js/example-plugin-admin.js'],
+				dest: ['<%= paths.authorAssets %>js/<%= pkg.name %>-admin.js']
+			},
+			{
+				src: ['<%= paths.authorAssets %>scss/example-plugin.scss'],
 				dest: ['<%= paths.authorAssets %>scss/<%= pkg.name %>.scss']
+			},
+			{
+				src: ['<%= paths.authorAssets %>scss/example-plugin-admin.scss'],
+				dest: ['<%= paths.authorAssets %>scss/<%= pkg.name %>-admin.scss']
 			}
 		]
 	}
