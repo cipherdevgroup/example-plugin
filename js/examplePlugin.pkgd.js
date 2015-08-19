32dfdde547bc49e5b0c4b94ff91f2cdeebce4eaa
robneu <rob@wpsitecare.com>
2015-08-18 23:04:04 -0500

    GTFO

--- js/examplePlugin.pkgd.js
+++
@@ -1,9 +0,0 @@
-/**
- * Example Plugin Public JavaScript
- *
- * @copyright Copyright 2015, WP Site Care
- * @license   MIT
- */
-( function( window, $, undefined ) {
-	'use strict';
-})( this, jQuery );
