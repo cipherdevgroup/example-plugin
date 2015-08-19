ac9130797d9a2f2cc0b71988643c77be4d4550df
robneu <rob@wpsitecare.com>
2015-08-18 21:45:36 -0500

    Added plugin action hooks

--- example-plugin.php
+++ example-plugin.php
@@ -16,8 +16,9 @@
 defined( 'ABSPATH' ) || exit;
 
 // Load the main plugin class.
-require_once plugin_dir_path( __FILE__ ) . 'class-plugin.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin.php';
 
+add_action( 'plugins_loaded', array( example_plugin(), 'run' ) );
 /**
  * Access a single instance of the main plugin class. Plugins and themes should
  * use this function to access plugin properties and methods. It's also a good
@@ -29,23 +30,61 @@ require_once plugin_dir_path( __FILE__ ) . 'class-plugin.php';
  * @return object Example_Plugin A single instance of the main plugin class.
  */
 function example_plugin() {
-	return Example_Plugin::instance();
+	return Example_Plugin::instance( array( 'file' => __FILE__ ) );
 }
 
 /**
- * Hook the main plugin class instance into plugins_loaded.
+ * Grab an instance of one of the plugin class objects.
  *
- * @since  0.1.0
- * @access public
- * @return void
+ * If you need to reference a method in one of the plugin classes, you should
+ * typically do it using this function.
+ *
+ * Example:
+ *
+ * <?php example_plugin_get( 'public-scripts' )->maybe_disable(); ?>
+ *
+ * @since   0.1.0
+ * @return  object
  */
-add_action( 'plugins_loaded', array( example_plugin(), 'run' ) );
+function example_plugin_get( $object, $name = 'canonical', $args = array() ) {
+	return Example_Plugin_Factory::get( $object, $name, $args );
+}
 
+register_activation_hook( __FILE__, 'example_plugin_activate' );
 /**
- * Register an activation hook to run all necessary plugin setup procedures.
+ * Set up roles, options and required data on plugin activation.
  *
  * @since  0.1.0
  * @access public
  * @return void
  */
-register_activation_hook( __FILE__, array( example_plugin(), 'activate' ) );
+function example_plugin_activate() {
+	example_plugin()->run();
+	example_plugin_get( 'plugin-hooks' )->activate();
+}
+
+register_deactivation_hook( __FILE__, 'example_plugin_deactivate' );
+/**
+ * Remove unnecessary data on plugin deactivation.
+ *
+ * @since  0.1.0
+ * @access public
+ * @return void
+ */
+function example_plugin_deactivate() {
+	example_plugin()->run();
+	example_plugin_get( 'plugin-hooks' )->deactivate();
+}
+
+register_uninstall_hook( __FILE__, 'example_plugin_uninstall' );
+/**
+ * Clean up all leftover roles, options, and data on plugin removal.
+ *
+ * @since  0.1.0
+ * @access public
+ * @return void
+ */
+function example_plugin_uninstall() {
+	example_plugin()->run();
+	example_plugin_get( 'plugin-hooks' )->uninstall();
+}
