e95a1f173071accf91bebf567e9971306d317ed3
robneu <rob@wpsitecare.com>
2016-06-24 13:49:16 -0400

    Improved readability

--- gruntfile.js
+++ gruntfile.js
@@ -2,8 +2,11 @@
 
 module.exports = function( grunt ) {
 	'use strict';
-	var config = require( 'sitecare-plugin-config' );
-	require( 'load-project-config' )( grunt, config ).init({
+
+	var loader = require( 'load-project-config' ),
+		config = require( 'sitecare-plugin-config' );
+
+	loader( grunt, config ).init({
 		pkg: grunt.file.readJSON( 'package.json' )
 	});
 };
